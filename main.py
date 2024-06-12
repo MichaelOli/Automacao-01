1042c19c494513af455b8d2ca38ca9f1b7d4c977
MichaelOli <michaeloliveira38@gmail.com>
2024-06-11 21:21:09 -0300

    Versão1.1

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 #Importando Biblioteca
 import pyautogui as auto
 import time
-
+#Define espera para cada comando
+auto.PAUSE = 1.5
 #Abre menu inicar do windows
 auto.press('win')
-time.sleep()
+
 #Abre o Chrome
 auto.write('chrome')
-time.sleep(3)
+
 auto.press('enter')
-time.sleep(2)
+
 # Maximizar tela
 auto.hotkey('alt', 'space',)
 auto.press('x')
-time.sleep(5)
 auto.write('github.com')
 auto.press('enter')
 
